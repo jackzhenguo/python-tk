e964a9457dfcb052088ed0c8bb942f3efd9c12f8
unknown <guozhen3@360buyad.local>
2018-02-17 19:23:12 +0800

    revise an official tk

--- src/official_tk_example.py
+++ src/official_tk_example.py
@@ -1,13 +1,13 @@
-import tkinter
+from tkinter import *
 from tkinter.constants import *
 
-tk = tkinter.Tk()
-frame = tkinter.Frame(tk, relief=RIDGE, borderwidth=2)
+tk = Tk()
+frame = Frame(tk, relief=RIDGE, borderwidth=2)
 frame.pack(fill=BOTH, expand=1)
-label = tkinter.Label(frame, text="Hello, World")
+label = Label(frame, text="Hello, World")
 label.pack(fill=X, expand=1)
 
 
-button = tkinter.Button(frame, text="Exit", command=tk.destroy)
+button = Button(frame, text="Exit", command=tk.destroy)
 button.pack(side=BOTTOM)
 tk.mainloop()
